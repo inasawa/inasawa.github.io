5f5ba18188d7c0d617c26426dd88197bf170dce6
inasawa <yuichi.inasawa@gmail.com>
2026-03-21 12:40:15 +0900

    webpush update

--- webpush/service-worker.js
+++ webpush/service-worker.js
@@ -1,4 +1,24 @@
-// public/custom-service-worker.js
+async function networkFirst(request) {
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open("MyCache_1");
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || Response.error();
+  }
+}
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname.match(/^\/inbox/)) {
+    event.respondWith(networkFirst(event.request));
+  }
+});
+
 self.addEventListener('push', event => {
     console.log('addEventListener push', event);
     const message = event.data.json();
